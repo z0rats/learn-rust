e8e96eaddee918668836d848ddd4fd9247238ce7
v0xat <v0xt.alex@gmail.com>
2022-12-14 18:20:53 +0300

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
+
+
+    let x = five();
+    println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
