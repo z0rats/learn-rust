5de61816712012012026b18a71bf8ff20eaceb87
v0xat <v0xt.alex@gmail.com>
2022-12-18 16:20:19 +0300

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,28 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+struct AlwaysEqual;
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anotheremail@example.com");
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+    
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+}
